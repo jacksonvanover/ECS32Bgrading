ed23fd211902c0397f9aad2d9151bc69e718b11a
jacksonvanover <jacksonvanover@gmail.com>
2019-02-17 22:24:38 -0800

    reorganized, added hw3 grader, modified readme

--- README.md
+++ README.md
@@ -1,26 +1,18 @@
 # hw2grading
 
 ### Requirements
-You will have to download each student assignment into the directory containing `grader.py` and `nodes.py`.
+You will have to download each student assignment into the directory containing `grader.py` and it's dependencies.
 Then, simply run `python grader.py`.
 
-In order for this to work, the student assignments must be named `hw2.py`. 
+In order for this to work, the student assignments must be named `hw1.py`, `hw3.py`, etc... 
 
 ### Usage
 
-Please remember that this is not the final test to see how well a student did on the assignment. It's more like a tool that will help you decide how carefully you need to look at the student's code. If everything passes, you won't have to look so hard.
-
-Regarding the slice method; there's a test in there but it will only work if the student included a working UnorderedList class. Enough students seemed to do this on their own so I included the test. Otherwise, you'll have to grade it by hand.
+Please remember that these are not the final test to see how well a student did on the assignment. It's more like a tool that will help you decide how carefully you need to look at the student's code. If everything passes, you won't have to look so hard.
  
- `[PASSED]` means the class works as expected; just glance at their implementation to make sure it looks good.
+ `[PASSED]` means the code works as expected; just glance at their implementation to make sure it looks good.
  
- `[ERROR]` means there are some problems with the class. The issues should be listed directly above the tag.
+ `[ERROR]` means there are some problems with the code. The issues should be listed directly above the tag.
  
- There are two types of issues:
- 
-   `ERROR:root:_____crashed!` tags mean some sort of exception occurred. This could be a syntax problem, name discrepancies, the code could just flat out not be there, etc. The method where the exception occurred is listed above the tag.
-
-   `<method name> mismatch` indicates that the method in question didn't return the proper output.
-
   
  Hopefully this is more of a help than a hindrance! Good luck.
---
+++ hw3/grader.py
@@ -0,0 +1,200 @@
+with open("hw3.py", 'r+') as f:
+    if f.readline().strip() != "from nodes import *":
+        f.seek(0,0)
+        content = f.read()
+        f.seek(0, 0)
+        f.write("from nodes import *".rstrip('\r\n') + '\n' + content)
+
+from hw3 import *
+from nodes import *
+import logging
+
+def main():
+    if prob1() == 0:
+        print("****************************************************************#1 Kleinfeldt [PASSED]\n")
+    else:
+        print("*****************************************************************#1 Kleinfeldt [ERROR]\n")
+    if prob2() == 0:
+        print("********************************************************************#2 Ladder [PASSED]\n")
+    else:
+        print("*********************************************************************#2 Ladder [ERROR]\n")
+    if prob3() == 0:
+        print("***************************************************************#3 findLargest [PASSED]\n")
+    else:
+        print("****************************************************************#3 findLargest [ERROR]\n")
+    if prob4() == 0:
+        print("**********************************************************#4 Majority Element [PASSED]\n")
+    else:
+        print("***********************************************************#4 Majority Element [ERROR]\n")
+    if prob5() == 0:
+        print("*****************************************************************#5 findValue [PASSED]\n")
+    else:
+        print("******************************************************************#5 findValue [ERROR]\n")
+    if prob5() == 0:
+        print("*************************************************************#6 findLastValue [PASSED]\n")
+    else:
+        print("**************************************************************#6 findLastValue [ERROR]\n")
+
+def prob1():
+    flag = 0
+    try:
+        if kleinfeldt(1) != 1:
+            print(kleinfeldt(1))
+            flag += 1
+        if kleinfeldt(2) != 1.25:
+            print(kleinfeldt(2))
+            flag += 1
+        if round(kleinfeldt(5), 7) != 1.4636111:
+            print(kleinfeldt(5))
+            flag += 1
+        if round(kleinfeldt(100),7) != 1.6349839:
+            print(kleinfeldt(100))
+            flag += 1
+        return flag
+    except:
+        return crash(flag)
+
+def prob2():
+    flag = 0
+    try:
+        if ladder(1) != 1:
+            flag += 1
+        if ladder(2) != 2:
+            flag += 1
+        if ladder(3) != 3:
+            flag += 1
+        if ladder(4) != 5:
+            flag += 1
+        if ladder(5) != 8:
+            flag += 1
+        if ladder(13) != 377:
+            flag += 1
+        return flag
+    except:
+        return crash(flag)
+
+def prob3():
+    flag = 0
+    a = [6,8,2,34,6,7,94,234,2,89,67,2,34]
+    try:
+        if findLargest(a) != 234:
+            flag += 1
+        return flag
+    except:
+        return crash(flag)
+
+def prob4():
+    flag = 0
+    a1 = [1,4,1,4]
+    a2 = [1,4,1,4,1]
+    a3 = [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,5]
+    a4 = [2,7,2,7,2,7,2,7,2,7,2,7,2,7]
+    a5 = [1,2,1,3,1,9,1,1,1,4,5,1,1,6,6,7,7,1,1]
+
+    try:
+        b1 = findPossibilities(a1)
+        b2 = findPossibilities(a2)
+        b3 = findPossibilities(a3)
+        b4 = findPossibilities(a4)
+        b5 = findPossibilities(a5)
+
+        if b1 != []:
+            print("FIND ERROR")
+            print("Was expecting [] but got ", b1)
+            flag += 1
+        if b2 != [1]:
+            print("FIND ERROR")
+            print("Was expecting [1] but got ", b2)
+            flag += 1
+        if b3 != [3,5]:
+            print("FIND ERROR")
+            print("Was expecting [3,5] but got ", b3)
+            flag += 1
+        if b4 != []:
+            print("FIND ERROR")
+            print("Was expecting [] but got ", b4)
+            flag += 1
+        if b5 != [1,1]:
+            print("FIND ERROR")
+            print("Was expecting [1,1] but got ", b5)
+            flag += 1
+
+        c1 = verifyPossibilities(a1, [])
+        c2 = verifyPossibilities(a2, [1])
+        c3 = verifyPossibilities(a3,[3,5])
+        c4 = verifyPossibilities(a4, [])
+        c5 = verifyPossibilities(a5, [1,1])
+
+        if c1 != None:
+            print("VERIFY ERROR")
+            print("Was expecting None but got ", c1)
+            flag += 1
+        if c2 != 1:
+            print("VERIFY ERROR")
+            print("Was expecting 1 but got ", c2)
+            flag += 1
+        if c3 != 3:
+            print("VERIFY ERROR")
+            print("Was expecting 3 but got ", c3)
+            flag += 1
+        if c4 != None:
+            print("VERIFY ERROR")
+            print("Was expecting None but got ", c4)
+            flag += 1
+        if c5 != 1:
+            print("VERIFY ERROR")
+            print("Was expecting 1 but got ", c5)
+            flag += 1
+
+        return flag
+
+    except:
+        return crash(flag)
+
+def prob5():
+    n1 = Node('A')
+    n2 = Node('B')
+    n3 = Node('C')
+    n4 = Node('D')
+    n3.setNext(n4)
+    n2.setNext(n3)
+    n1.setNext(n2)
+
+    flag = 0
+
+    try:
+        if findValue('A', n1) != True:
+            flag +=1
+        if findValue('D', n1) != True:
+            flag +=1
+        if findValue(9, n1) != False:
+            flag +=1
+        return flag
+    except:
+        return crash(flag)    
+
+def prob6():
+    n1 = Node('A')
+    n2 = Node('B')
+    n3 = Node('C')
+    n4 = Node('D')
+    n3.setNext(n4)
+    n2.setNext(n3)
+    n1.setNext(n2)
+
+    flag = 0
+
+    try:
+        if findLastValue(n1) != 'D':
+            flag += 1
+        return flag
+    except:
+        return crash(flag)
+
+def crash(flag):
+    logging.exception("_____crashed!")
+    flag += 1
+    return flag
+
+if __name__ == "__main__":
+    main()
