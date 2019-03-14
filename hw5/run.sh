a1d33a7d9f28856b3b9ca7b60962d7a7bf01558e
jacksonvanover <jacksonvanover@gmail.com>
2019-03-14 07:09:38 -0700

    updated names from hw4 to hw5

--- hw5/run.sh
+++ hw5/run.sh
@@ -1,5 +1,5 @@
 rm hw5.py
-ls | grep -v 'run.sh\|grader.py\|__pycache__' | tr -d '\n' | xargs -0 -I {} mv {} hw4.py
+ls | grep -v 'run.sh\|grader.py\|__pycache__' | tr -d '\n' | xargs -0 -I {} mv {} hw5.py
 sleep 1
 python3 grader.py
 code hw5.py
